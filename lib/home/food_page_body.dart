a5615a5c2080dde0a9afa965039787d0e563a51d
fidelis <fidelisnwabisi@gmail.com>
2023-06-02 10:44:07 +0100

    finished animating the slider transition

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -15,6 +15,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
   var _currPageValue = 0.0;
   double _scaleFactor = 0.8;
+  double _height = 220;
 
   @override
   void initState() {
@@ -22,7 +23,6 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     pageController.addListener(() {
       setState(() {
         _currPageValue = pageController.page!;
-        print("Current value is " + _currPageValue.toString());
       });
     });
   }
@@ -51,89 +51,119 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     Matrix4 matrix = new Matrix4.identity();
     if (index == _currPageValue.floor()) {
       var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
     } else if (index == _currPageValue.floor() + 1) {
-      var currScale = _scaleFactor + (_currPageValue - index + 1);
+      var currScale =
+          _scaleFactor + (_currPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() - 1) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 1);
     }
-    return Stack(children: [
-      Container(
-        height: 220,
-        margin: const EdgeInsets.only(left: 10, right: 10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color:
-              index.isEven ? const Color(0xFF69c5df) : const Color(0xFF9294cc),
-          image: const DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage("assets/image/food0.png"),
-          ),
-        ),
-      ),
-      Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          height: 120,
-          margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+
+    return Transform(
+      transform: matrix,
+      child: Stack(children: [
+        Container(
+          height: 220,
+          margin: const EdgeInsets.only(left: 10, right: 10),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
-            color: Colors.white,
-          ),
-          child: Container(
-            padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const BigText(text: "Chinese Side"),
-                const SizedBox(height: 10),
-                Row(
-                  children: [
-                    Wrap(
-                      children: List.generate(
-                        5,
-                        (index) {
-                          return Icon(
-                            Icons.star,
-                            color: AppColors.mainColor,
-                            size: 15,
-                          );
-                        },
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    const SmallText(text: "4.5"),
-                    const SizedBox(width: 10),
-                    const SmallText(text: "1287"),
-                    const SizedBox(width: 10),
-                    const SmallText(text: "Comments"),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    // Reusable Icon and Text Widget
-                    IconAndTextWidget(
-                      icon: Icons.circle_sharp,
-                      text: "Normal",
-                      iconColor: AppColors.iconColor1,
-                    ),
-
-                    IconAndTextWidget(
-                      icon: Icons.location_on,
-                      text: "1.7km",
-                      iconColor: AppColors.mainColor,
-                    ),
-                    IconAndTextWidget(
-                      icon: Icons.access_time_rounded,
-                      text: "31min",
-                      iconColor: AppColors.iconColor2,
-                    ),
-                  ],
-                ),
-              ],
+            color: index.isEven
+                ? const Color(0xFF69c5df)
+                : const Color(0xFF9294cc),
+            image: const DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage("assets/image/food0.png"),
             ),
           ),
         ),
-      ),
-    ]);
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFFe8e8e8),
+                    blurRadius: 5.0,
+                    offset: Offset(0, 5),
+                  ),
+                ]),
+            child: Container(
+              padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const BigText(text: "Chinese Side"),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) {
+                            return Icon(
+                              Icons.star,
+                              color: AppColors.mainColor,
+                              size: 15,
+                            );
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      const SmallText(text: "4.5"),
+                      const SizedBox(width: 10),
+                      const SmallText(text: "1287"),
+                      const SizedBox(width: 10),
+                      const SmallText(text: "Comments"),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      // Reusable Icon and Text Widget
+                      IconAndTextWidget(
+                        icon: Icons.circle_sharp,
+                        text: "Normal",
+                        iconColor: AppColors.iconColor1,
+                      ),
+
+                      IconAndTextWidget(
+                        icon: Icons.location_on,
+                        text: "1.7km",
+                        iconColor: AppColors.mainColor,
+                      ),
+                      IconAndTextWidget(
+                        icon: Icons.access_time_rounded,
+                        text: "31min",
+                        iconColor: AppColors.iconColor2,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
   }
 }
